b20ea85fd98a570bb5caeff4c4ab050c42ad14c4
Alexander Augusto <alexaasf_10@hotmail.com>
2021-03-31 15:23:22 -0300

    create sql script

---
+++ api/db/script.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS programadorabordo;
+
+USE programadorabordo;
+
+CREATE TABLE IF NOT EXISTS products (
+  id INT(11) AUTO_INCREMENTE,
+  name VARCHAR(255),
+  price DECIMAL(10, 2),
+  PRIMARY KEY (id)
+);
+
+INSERT INTO products VALUE(0, 'Curso Front-end especialista', 2500);
+INSERT INTO products VALUE(0, 'Curso JS Fullstack', 900);
